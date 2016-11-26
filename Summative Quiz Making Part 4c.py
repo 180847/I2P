e6d7120a536f59e1b0519fd78e06999e23130567
180847 <180847@hkis.edu.hk>
2016-11-27 00:56:38 +0800

    nothing is working now

--- Summative Quiz Making Part 4c.py
+++ Summative Quiz Making Part 4c.py
@@ -5,7 +5,7 @@ x = [i for i in range(18)]
 shuffle(x)
 counter=0
 RandomVariable1=x[counter]
-global realanswer
+#global realanswer
 
 name = input("What is your name?")
 print ("Welcome",name,"Looks like you are taking this Knowledge test, have fun!(Calculators are not allowed)")
@@ -17,40 +17,45 @@ Score = 0
 questionnumber = 0
 questionsasked = 0
 
-def askquestion(questionnumber):
-    global questionsasked
-    while questionsasked >= 0 and questionsasked < 17:
-        global Score
-        global RandomVariable1
-        print (RandomVariable1)
+#def askquestion(questionnumber):
+#global questionsasked
+while questionsasked >= 0 and questionsasked < 17:
+    #global Score
+    #global RandomVariable1
+    print (RandomVariable1)
         #for line in range (2,3):
-        for i, line in enumerate(file):
-            if i==RandomVariable1:
-                print(file.readline())
-        for i, line in enumerate(file2):
-            if i == RandomVariable1:
-                realanswer=file2.readline()
+    for i, line in enumerate(file):
+        if i==RandomVariable1:
+            print(file.readline())
+        if i==RandomVariable1:
+            print(file.readline())
+    for i, line in enumerate(file2):
+        if i == RandomVariable1:
+            realanswer=file2.readline()
                 #print(file2.readline())
-                print(realanswer)
-        answer = input("Input Letter")
-        answer=str(answer)
-        answer=answer.upper()
+            print(realanswer)
+    answer = input("Input Letter")
+    answer=str(answer)
+    answer=answer.upper()
         #realanswer=file2.readline()
-        #realanswer=realanswer[0]
-        realanswer=str(realanswer)
-        if realanswer==answer:
-            Score=Score+1
-            print ("You are correct. Your score is: ", str(Score))
-            questionsasked=questionsasked+1
-            askquestion(questionnumber + 1)
-        else:
-            print ("You were incorrect. The correct answer is:", realanswer)
-            print ("Your score is: ", str(Score))
-            questionsasked=questionsasked+1
-            askquestion(questionnumber + 1)
+    realanswer=realanswer[0]
+    realanswer=str(realanswer)
+    if realanswer==answer:
+        Score=Score+1
+        print ("You are correct. Your score is: ", str(Score))
+        questionsasked=questionsasked+1
+        #askquestion(questionnumber + 1)
         counter=counter+1
-        RandomVariable1=x(counter)
-askquestion(questionnumber)
+        RandomVariable1=x[counter]
+    else:
+        print ("You were incorrect. The correct answer is:", realanswer)
+        print ("Your score is: ", str(Score))
+        questionsasked=questionsasked+1
+        #askquestion(questionnumber + 1)
+        counter=counter+1
+        RandomVariable1=x[counter]
+
+#askquestion(questionnumber)
 
 print (name, ",", str(Score), "out of 17 was your score, completed on", date)
 file3=open("Highscores", "r+")
